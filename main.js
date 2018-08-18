280b60dc136db30cddfe2c70ce81aec5072256b4
Petar Stevovski <stevovski.petar@gmail.com>
2018-08-18 02:57:59 +0200

    Dynamically change assets according to time, updates to the code & fixes

--- main.js
+++ main.js
@@ -15,63 +15,54 @@ const scoreSound = new Audio();
 const flySound = new Audio();
 const dieSound = new Audio();
 const hitSound = new Audio();
-scoreSound.src = "sounds/score.mp3";
-flySound.src = "sounds/fly.mp3";
+scoreSound.src = "sounds/point.ogg";
+flySound.src = "sounds/wing.ogg";
 dieSound.src = "sounds/die.ogg";
 hitSound.src = "sounds/hit.ogg";
 
 // Global variables
-let gravity = 1.5; // Force that pulls the bird down
-let gap = 100;
+let gravity = 2; // Force that pulls the bird down
+let gap = 100; // Gap between the pipes
 let constant; // The sum of the top pipe height + gap.
-let score = 0;
+let score = 0; // Starting score
+let checkGameStatusInterval = setInterval(checkGameStatus, 50);
+let d; // Direction
+let playing = false; // Flag variable
+let checkGame = false; // Flag variable
+let game;
+
 const displayScore = document.getElementById("score");
 const day = document.getElementById("day");
 const night = document.getElementById("night");
 const container = document.querySelector(".container");
-let d; // Direction
 
-// Start 
-document.getElementById("startGame").addEventListener("click", ()=>{
-    document.getElementById("startGame").style.display = "none";
-    document.querySelector(".levels").style.display = "flex";
-})
-let test;
-// Day mode
-day.addEventListener("click", ()=>{
-    // Hide buttons
-    document.querySelector(".levels").style.display = "none";
-    // Display container
-    container.style.display = "flex";
+// Get the current hour and if its daytime, display day background, if its night time, display night background.
+let today = new Date().getHours();
+if ( today >= 7 && today <= 20){
     // Chosen background
     bg.src = "images/background-day.png";
     // Chosen ground
     ground.src = "images/fg.png";
     // Chosen pipes
     pipeTop.src = "images/pipeNorth.png";
-    pipeBottom.src = "images/pipeSouth.png";  
-    draw();
-})
-// Night mode
-night.addEventListener("click", ()=>{
-    // Hide buttons
-    document.querySelector(".levels").style.display = "none";
-    displayScore.style.color = "#f2f2f2";
-    // Display container
-    container.style.display = "flex";
+    pipeBottom.src = "images/pipeSouth.png"; 
+} else if ( today >= 20 || today < 7) {
     // Chosen background
-    bg.src= "images/backgroundNight.png";
+    bg.src = "images/backgroundNight.png";
     // Chosen ground
     ground.src = "images/groundNight.png";
-    // Pipes
+    // Chosen pipes
     pipeTop.src = "images/pipeTopNight.png";
     pipeBottom.src = "images/pipeBottomNight.png";
-    draw();
-})
+    // Change score color
+    displayScore.style.color = "#f2f2f2";
+    // Change body color
+    document.querySelector("body").style.backgroundColor = "#2f3640";
+}
 
 // Bird position
 let birdX = 20;
-let birdY = 150;
+let birdY = 230;
 
 // Pipe(s)
 let pipe = [];
@@ -80,31 +71,52 @@ pipe[0] = {
     y: 0
 }
 
-// Bird jumps (fly)
+// Keyboard & touch (for mobile devices) event listeners.
 document.addEventListener("keydown", jump);
-document.addEventListener("keyup", changeStatus);
+document.addEventListener("keyup", jumpStatus);
 document.addEventListener("touchstart", touchJump);
 document.addEventListener("touchend", touchStatus);
+
+// On page load initialization
+window.onload = function init(){
+    draw();
+    playing = true;
+    checkGameStatusInterval;
+}
+// Bird jumps on space press.
 function jump(e){
-    // Space key code is 32.
-    if ( e.keyCode === 32) {
-        birdY -= 30;
+    if ( playing == true && e.code === "Space"){
+        checkGame = true;
+        // Space key is 32.
+        birdY -= 50;
         d = "UP";
         flySound.play();
     }
 }
-function changeStatus(){
+// If no button is pressed, clears the direction of the bird.
+function jumpStatus(){
     d = "";
 }
+// Bird jumps on screen touch.
 function touchJump(){
-    birdY -=30;
+    birdY -= 50;
     d = "UP";
+    flySound.play();
+    checkGame = true;
 }
+// If screen is not touched, clears the direction of the bird.
 function touchStatus(){
     d = "";
 }
-displayScore.innerHTML = "Score: 0";
-
+// Checks the game status to see if the flag variable (checkGame) is true - representing player tapped on screen / touched spacebar to start.
+function checkGameStatus(){
+    if( checkGame == true) {
+        clearInterval(checkGameStatusInterval)
+        document.querySelector(".deviceInfo").style.display = "none";
+        game = setInterval(draw, 1000/60);
+    }
+}
+// Updates the image shown for the bird according to the status (flying / falling).
 function changeBird(){
     if ( d == "UP") {
         bird.src = "images/birdUp.png";
@@ -113,6 +125,8 @@ function changeBird(){
         bird.src = "images/birdDown.png";
     }
 }
+// Display the score on start.
+displayScore.innerHTML = score;
 function draw(){
     // Change bird image
     changeBird();
@@ -138,10 +152,10 @@ function draw(){
         }
 
         // Increase score
-        if ( pipe[i].x == 10) {
+        if ( pipe[i].x == 5) {
             scoreSound.play();
             score++;
-            displayScore.innerHTML = "Score: "+score;
+            displayScore.innerHTML = score;
         }
 
         // Detect collision with pipes and ground.
@@ -150,8 +164,8 @@ function draw(){
             bird.src = "images/deadBird.png";
             setTimeout(() => {
                 clearInterval(game);
+                gameOver();
             }, 30);
-            gameOver();
         }
     }
     // Draw the ground
@@ -162,25 +176,48 @@ function draw(){
     birdY += gravity;
 }
 
-let game = setInterval(draw, 1000/60);
-
+const gameOverMenu = document.querySelector(".gameOver");
 function gameOver(){
     dieSound.play();
     // Display game over menu
-    document.querySelector(".gameOver").style.display = "block";
+    gameOverMenu.style.display = "block";
+    gameOverMenu.classList.add("active");
     // Display score
     document.getElementById("showScore").innerHTML = score;
     // Display highscore
     let highscore = localStorage.getItem("highscoreBird")
     document.getElementById("showHighscore").innerHTML = highscore;
-    // Display highscore.
+    // Update highscore if the current score is higher than the previous highscore.
     if ( score > highscore) {
         localStorage.setItem("highscoreBird", score);
         let newHighscore = localStorage.getItem("highscoreBird");
         document.getElementById("showHighscore").innerHTML = newHighscore;
     }
+    playing = false;
 }
 // Play again
 document.getElementById("playAgain").addEventListener("click", ()=>{
+    // location.reload();
+    document.querySelector(".gameOver").style.display = "none";
+    // Clear the pipe array of all the pipes accumulated during previous game.
+    pipe.splice(0, pipe.length);
+    // Reset first pipe position.
+    pipe[0] = {
+        x: cvs.width,
+        y: 0
+    }
+    // Reset birds initial position.
+    birdX = 20;
+    birdY = 150;
+    // Reset score
+    score = 0;
+    displayScore.innerHTML = score;
+    // Set the boolean playing to true
+    playing = true;
+    game = setInterval(draw, 1000/60);
+})
+
+// Exit game to main menu
+document.getElementById("exit").addEventListener("click", ()=>{
     location.reload();
 })
