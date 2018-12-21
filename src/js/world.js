6b2a2843ed2f5e2880689fac3a578f05ff8b3b68
Petar Stevovski <stevovski.petar@gmail.com>
2018-12-21 02:15:45 +0100

    Corrections in gravity, added game initialization and end game methods.

--- src/js/world.js
+++ src/js/world.js
@@ -1,45 +1,74 @@
 import { ui } from "./ui.js";
 import { player } from "./player.js";
 import { bird } from "./bird.js";
+import { sfx, gfx } from "./assets.js";
 
 class World {
     constructor() {
-        this.gravity = 2.5;
+        this.gravity = 2.3;
         this.gap = 90;
         this.constant = null;
-        this.game = null;
+        this.update = null;
+        this.cvs = document.querySelector("#canvas");
+        this.ctx = this.cvs.getContext("2d");
 
         // Pipes
         this.pipes = [];
         this.pipes[0] = {
-            x: ui.cvs.width,
+            x: this.cvs.width,
             y: 0
         }
     }
 
     // Initialize game
     init() {
+        // Hide the game over menu
+        ui.gameOver.style.display = "none";
+
+        // Bird's image
+        gfx.bird.src = "images/bird.png";
+
+        // Reset the pipes and clear the update variable
         this.pipes.splice(0, this.pipes.length);
+        this.pipes[0] = {
+            x: this.cvs.width,
+            y: 0
+        }
+        this.update = null;
+        
+        // Reset player score and status
         player.score = 0;
         player.playing = true;
 
+        // Reset bird's status
         bird.x = 20;
         bird.y = 230;
         bird.d = null;
 
+        // Display starting score
         ui.scoreDisplay.textContent = player.score;
-
     }
 
-    // // Game status
-    // gameStatus() {
-    //     // If its already true, exit the function
-    //     if(player.gameRunning) return; 
+    // Game status
+    deviceInfo() {
+        // Hide the device info text
+        ui.deviceInfo.style.display = "none";
+    }
 
-    //     // clearInterval(checkGameStatusInterval)
-    //     ui.deviceInfo.style.display = "none";
-    //     this.game = setInterval(draw, 1000/60);
-    // }
+    // End the game
+    endGame() {
+        cancelAnimationFrame(this.update);
+        // Change bird's image
+        gfx.bird.src = "images/deadBird.png";
+
+        // Play dead sound
+        sfx.dieSound.play();
+
+        // Display game over menu
+        ui.gameOverMenu();
+
+        player.playing = false;
+    }
 }
 
 export const world = new World();
